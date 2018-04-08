883d88c65960a9750ef6985fc7bd52e28fef1933
Lior Tal <lior@moonactive.com>
2018-04-08 17:34:04 +0300

    Update Graph API version to the latest
    Set GraphApiVersion to the latest one available as of today (2.12)

--- Facebook.Unity/Constants.cs
+++ Facebook.Unity/Constants.cs
@@ -44,7 +44,7 @@ namespace Facebook.Unity
         public const string OnGroupJoinCompleteMethodName = "OnJoinGroupComplete";
 
         // Graph API
-        public const string GraphApiVersion = "v2.6";
+        public const string GraphApiVersion = "v2.12";
         public const string GraphUrlFormat = "https://graph.{0}/{1}/";
 
         // Permission Strings

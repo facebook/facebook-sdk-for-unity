ab64c0c83a7638a07ef2182a0bca957175d43029
Théo Sabattié <theo.sabattie@gmail.com>
2019-05-31 14:14:59 +0200

    use USERPROFILE environment variable instead of HOMEDRIVE and HOMEPATH

--- Facebook.Unity.Editor/android/FacebookAndroidUtil.cs
+++ Facebook.Unity.Editor/android/FacebookAndroidUtil.cs
@@ -94,7 +94,7 @@ namespace Facebook.Unity.Editor
             get
             {
                 return (Application.platform == RuntimePlatform.WindowsEditor) ?
-                    System.Environment.GetEnvironmentVariable("HOMEDRIVE") + System.Environment.GetEnvironmentVariable("HOMEPATH") + @"\.android\debug.keystore" :
+                    System.Environment.GetEnvironmentVariable("USERPROFILE") + @"\.android\debug.keystore" :
                     System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal) + @"/.android/debug.keystore";
             }
         }

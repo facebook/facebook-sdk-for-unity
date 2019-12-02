28ce7db6ed13e5298edd0fb9459f9225ecc10c6d
Zilin Zhang <kylinchang@fb.com>
2019-12-02 10:57:30 -0800

    Add travis configuration
    Summary: As title
    
    Reviewed By: tianqibt
    
    Differential Revision: D18746599
    
    fbshipit-source-id: 2e2fd3047d12cc88b482fbd098474aa50d7dd2ff

--- scripts/common.sh
+++ scripts/common.sh
@@ -86,7 +86,7 @@ downloadUnityJarResolverFromGithub() {
 
   UNITY_PACKAGE_PATH="$PROJECT_ROOT/$UNITY_JAR_RESOLVER_PACKAGE"
 
-  $UNITY_PATH -quit -batchmode -logFile -projectPath="$PROJECT_ROOT/UnitySDK" \
+  $UNITY_PATH -quit -batchmode -logFile -nographics -projectPath="$PROJECT_ROOT/UnitySDK" \
    -importPackage "$UNITY_PACKAGE_PATH" || die "Failed to import $UNITY_PACKAGE_PATH"
   info "Cleaning up..."
   rm "$UNITY_PACKAGE_PATH"

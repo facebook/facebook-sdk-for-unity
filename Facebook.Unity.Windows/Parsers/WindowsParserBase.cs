eeabb4a2657c75fe73e6285124f2d508b4aa0345
Jordi Ramon <jordiramon@fb.com>
2021-09-16 01:56:06 -0700

    Added Windows Parser Base class
    Summary:
    Added Windows Parser Base class to be extended by the parser.
    
    This base class will have common functions for  the catalog and purchase parsers.
    
    Reviewed By: ffgomezfb
    
    Differential Revision: D30963068
    
    fbshipit-source-id: 2989af8f8032b449f78aef7c00e6b146c1a165e3

---
+++ Facebook.Unity.Windows/Parsers/WindowsParserBase.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Collections.Generic;
+
+namespace Facebook.Unity.Windows
+{
+    class WindowsParserBase
+    {
+        public static ResultContainer SetError(fbg.Error error, string callbackId)
+        {
+            string msg = "ERROR: " + error.Message + ",";
+            msg += "InnerErrorCode: " + error.InnerErrorCode.ToString() + ",";
+            msg += "InnerErrorMessage: " + error.InnerErrorMessage + ",";
+            msg += "InnerErrorSubcode: " + error.InnerErrorSubcode.ToString() + ",";
+            msg += "InnerErrorTraceId: " + error.InnerErrorTraceId;
+
+            ResultContainer container = new ResultContainer(new Dictionary<string, object>()
+                {
+                    {Constants.CallbackIdKey,callbackId},
+                    {Constants.ErrorKey,msg }
+                }
+            );
+            return container;
+        }
+    }
+}

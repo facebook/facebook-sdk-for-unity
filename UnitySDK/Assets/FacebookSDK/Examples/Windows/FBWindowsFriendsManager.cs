710ba511bb894e111fbb837b19c7be22032f6606
Jordi Ramon <jordiramon@fb.com>
2021-09-27 10:11:07 -0700

    Windows Example: Friends request
    Summary: Added Friends Inviations management inside the Unity SDK Windows Example.
    
    Reviewed By: ffgomezfb
    
    Differential Revision: D31169813
    
    fbshipit-source-id: 6a9835e626599cca08aa8972c3d6415a289794d2

---
+++ UnitySDK/Assets/FacebookSDK/Examples/Windows/FBWindowsFriendsManager.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+using System;
+
+public class FBWindowsFriendsManager : MonoBehaviour {
+
+    public FBWindowsLogsManager Logger;
+    public Transform ReceivedInvitationsPanelTransform;
+    public GameObject ShowReceivedInvitation;
+
+    public void Button_OpenReceivedInvitations()
+    {
+        if (FB.IsLoggedIn)
+        {
+            FB.OpenFriendFinderDialog(OpenFriendsDialogCallBack);
+        }
+        else
+        {
+            Logger.DebugLog("Login First");
+        }
+    }
+
+    private void OpenFriendsDialogCallBack(IGamingServicesFriendFinderResult result)
+    {
+        if (result.Error != null)
+        {
+            Logger.DebugLog(result.Error);
+        }
+        else
+        {
+            Logger.DebugLog("Friends Dialog openned succesflly");
+        }
+    }
+
+    public void Button_GetFriendFinderInvitations()
+    {
+        if (FB.IsLoggedIn)
+        {
+            FB.GetFriendFinderInvitations(GetFriendFinderInvitationsCallback);
+        }
+        else
+        {
+            Logger.DebugLog("Login First");
+        }
+    }
+
+    private void GetFriendFinderInvitationsCallback(IFriendFinderInvitationResult receivedInvitations)
+    {
+        Logger.DebugLog("Processing received invitations...");
+
+        foreach (Transform child in ReceivedInvitationsPanelTransform)
+        {
+            Destroy(child.gameObject);
+        }
+
+        if (receivedInvitations.Error != null)
+        {
+            Logger.DebugLog(receivedInvitations.Error);
+        }
+        else
+        {
+            if (receivedInvitations.Invitations.Count <= 0)
+            {
+                Logger.DebugLog("No invitations received.");
+            }
+            else
+            {
+                foreach (FriendFinderInviation item in receivedInvitations.Invitations)
+                {
+                    GameObject obj = (GameObject)Instantiate(ShowReceivedInvitation);
+                    obj.transform.SetParent(ReceivedInvitationsPanelTransform, false);
+                    obj.transform.localScale = new Vector3(1, 1, 1);
+
+                    Button button = obj.GetComponent<Button>();
+                    button.onClick.AddListener(() => { button.interactable = false; FB.DeleteFriendFinderInvitation(item.Id, DeleteFriendFinderInvitationCallback); });
+
+                    Text textComponent = obj.GetComponentInChildren<Text>();
+                    textComponent.text = "ApplicationName: " + item.ApplicationName + "\nFrom: " + item.FromName + "\nTo: " + item.ToName + "\nMessage: " + item.Message + "\nTime: " + item.CreatedTime;
+                }
+            }
+        }
+    }
+
+    private void DeleteFriendFinderInvitationCallback(IFriendFinderInvitationResult receivedInvitations)
+    {
+        if (receivedInvitations.Error != null)
+        {
+            Logger.DebugLog(receivedInvitations.Error);
+        }
+        else
+        {
+            Logger.DebugLog("Inviation deleted succesfully. Give a reward at this point if you want.");
+        }
+        Button_GetFriendFinderInvitations();
+    }
+}

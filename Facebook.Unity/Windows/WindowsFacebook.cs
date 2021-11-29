888a90136caed769ccd7dc166e92628adc3bb147
Jordi Ramon <jordiramon@fb.com>
2021-11-29 05:29:11 -0800

    Updated Windows DLL with the purchase fix.
    Summary: Updated Windows SDK DLLs to 1.0.5 with the purchase fix and share link
    
    Reviewed By: ffgomezfb
    
    Differential Revision: D32559095
    
    fbshipit-source-id: 046cd72f025e5f5f69f8cd591c60ec2c047b28c5

--- Facebook.Unity/Windows/WindowsFacebook.cs
+++ Facebook.Unity/Windows/WindowsFacebook.cs
@@ -54,7 +54,7 @@ namespace Facebook.Unity.Windows
         {
             get
             {
-                return "1.0.3";
+                return "1.0.5";
             }
         }
         public void Init(

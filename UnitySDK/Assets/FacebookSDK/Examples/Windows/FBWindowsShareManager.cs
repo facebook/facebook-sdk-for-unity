58e302243a7710e1232c5626291a0497f692c10d
Paco Fernandez <ffgomez@fb.com>
2021-12-02 01:04:18 -0800

    Facebook Unity Windows Share Example
    Summary: Facebook Unity Share Example
    
    Reviewed By: JordiFB
    
    Differential Revision: D32760448
    
    fbshipit-source-id: b2da253b8c70e45ff1fd72c177a9e511b3bd1217

---
+++ UnitySDK/Assets/FacebookSDK/Examples/Windows/FBWindowsShareManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+using Facebook.MiniJSON;
+using System;
+using System.IO;
+
+public class FBWindowsShareManager : MonoBehaviour
+{
+
+    public FBWindowsLogsManager Logger;
+    public InputField Caption;
+    public InputField ImageFile;
+    public InputField VideoFile;
+    public Toggle ShouldShowDialog;
+    public Button ImageUploadButton;
+    public Button VideoUploadButton;
+
+    public void Button_UploadImage()
+    {
+        ImageUploadButton.interactable = false;
+        Logger.DebugLog("Uploading image file [" + Path.GetFullPath(ImageFile.text) + "] to media library");
+        FB.UploadImageToMediaLibrary(Caption.text, new Uri(Path.GetFullPath(ImageFile.text)), ShouldShowDialog.isOn, CallbackUploadImage);
+    }
+
+    public void Button_UploadVideo()
+    {
+        VideoUploadButton.interactable = false;
+        Logger.DebugLog("Uploading video file [" + Path.GetFullPath(VideoFile.text) + "] to media library");
+        FB.UploadVideoToMediaLibrary(Caption.text, new Uri(Path.GetFullPath(VideoFile.text)), ShouldShowDialog.isOn, CallbackUploadVideo);
+    }
+
+    private void CallbackUploadImage(IMediaUploadResult result)
+    {
+        ImageUploadButton.interactable = true;
+        if (result.Error != null)
+        {
+            Logger.DebugErrorLog("Image upload error: " + result.RawResult);
+            Logger.DebugErrorLog(result.Error);
+        }
+        else
+        {
+            Logger.DebugLog("Image uploaded. ID: " + result.MediaId);
+        }
+    }
+
+    private void CallbackUploadVideo(IMediaUploadResult result)
+    {
+        VideoUploadButton.interactable = true;
+        if (result.Error != null)
+        {
+            Logger.DebugErrorLog("Video upload error: " + result.RawResult);
+            Logger.DebugErrorLog(result.Error);
+        }
+        else
+        {
+            Logger.DebugLog("Video uploaded. ID: " + result.MediaId);
+        }
+    }
+}

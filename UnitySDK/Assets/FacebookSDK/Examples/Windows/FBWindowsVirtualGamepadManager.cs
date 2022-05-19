002003adb1f7d06575c300305cdef31ba397c108
Sergio Guerrero <sergioguerrero@fb.com>
2022-05-19 02:41:10 -0700

    Added SetVirtualGamepadLayout to the Windows Example
    Summary: Added a new tab for Virtual Gamepad methods and included a button to set a virtual gamepad layout by calling FB.Windows.SetVirtualGamepadLayout
    
    Reviewed By: JordiFB
    
    Differential Revision: D36482676
    
    fbshipit-source-id: 395923e1cf440a9032bf20cd956693348e39c24d

---
+++ UnitySDK/Assets/FacebookSDK/Examples/Windows/FBWindowsVirtualGamepadManager.cs
@@ -0,0 +1,29 @@
+﻿using Facebook.Unity;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FBWindowsVirtualGamepadManager : MonoBehaviour
+{
+    public FBWindowsLogsManager Logger;
+    public InputField NewVirtualGamepadLayout;
+
+    public void SetVirtualGamepadLayout()
+    {
+        Logger.DebugLog("Setting Virtual Gamepad Layout to " + NewVirtualGamepadLayout.text);
+        FB.Windows.SetVirtualGamepadLayout(NewVirtualGamepadLayout.text, CallbackSetVirtualGamepadLayout);
+    }
+
+    private void CallbackSetVirtualGamepadLayout(IVirtualGamepadLayoutResult result)
+    {
+        if (result.Error != null)
+        {
+            Logger.DebugErrorLog("Error setting Virtual Gamepad Layout: " + result.RawResult);
+            Logger.DebugErrorLog(result.Error);
+        }
+        else
+        {
+            Logger.DebugLog("Virtual Gamepad Layout set: " + result.Success);
+        }
+    }
+}

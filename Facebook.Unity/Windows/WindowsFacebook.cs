076f73cf5bbd247b3b222e5e676c5e4c3322a15f
Sergio Guerrero <sergioguerrero@fb.com>
2022-05-19 02:41:10 -0700

    Updated Windows SDK DLLs to 1.0.12
    Summary: Updated Windows SDK DLLs to version 1.0.12
    
    Reviewed By: JordiFB
    
    Differential Revision: D36482674
    
    fbshipit-source-id: 6b2365edc0057d0bc7c59092bd22d612ac7f4ed0

--- Facebook.Unity/Windows/WindowsFacebook.cs
+++ Facebook.Unity/Windows/WindowsFacebook.cs
@@ -54,7 +54,7 @@ namespace Facebook.Unity.Windows
         {
             get
             {
-                return "1.0.10";
+                return "1.0.12";
             }
         }
         public void Init(

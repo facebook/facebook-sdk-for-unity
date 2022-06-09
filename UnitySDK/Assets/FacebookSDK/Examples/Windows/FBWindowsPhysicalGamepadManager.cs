06ad090c43cd27c9bdebba21a1234fafdbfcdac2
Sergio Guerrero <sergioguerrero@fb.com>
2022-06-09 07:11:40 -0700

    Added a new tab in the example to check gamepad input
    Summary: Added a new tab 'Physical Gamepad' to check gamepad input using a XInput plugin. This example will help developers troubleshoot errors regarding gamepad input, as requested by the Super Animal Royale team.
    
    Reviewed By: JordiFB
    
    Differential Revision: D37037047
    
    fbshipit-source-id: 1581a087d75651c0d48c5c9e561ec104fa1175cf

---
+++ UnitySDK/Assets/FacebookSDK/Examples/Windows/FBWindowsPhysicalGamepadManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using XInputDotNetPure;
+
+public class FBWindowsPhysicalGamepadManager : MonoBehaviour {
+    [SerializeField]
+    private Text displayGamepadInputText;
+    private GamePadState state;
+
+    void Update()
+    {
+        state = GamePad.GetState(0);
+        displayGamepadInputText.text = "";
+        displayGamepadInputText.text += "--- Sticks ---\n";
+        displayGamepadInputText.text += string.Format("Left X: {0}\nLeft Y: {1}\nRight X: {2}\nRight Y: {3}\n", state.ThumbSticks.Left.X, state.ThumbSticks.Left.Y, state.ThumbSticks.Right.X, state.ThumbSticks.Right.Y);
+        displayGamepadInputText.text += "\n--- Buttons ---\n";
+        displayGamepadInputText.text += string.Format("Start: {0} | Back: {1}\n", state.Buttons.Start, state.Buttons.Back);
+        displayGamepadInputText.text += string.Format("A: {0} | B: {1}  | X: {2} | Y: {3}\n", state.Buttons.A, state.Buttons.B, state.Buttons.X, state.Buttons.Y);
+        displayGamepadInputText.text += string.Format("LeftShoulder: {0} | RightShoulder: {1}\n", state.Buttons.LeftShoulder, state.Buttons.RightShoulder);
+        displayGamepadInputText.text += string.Format("LeftStick: {0} | RightStick: {1}\n", state.Buttons.LeftStick, state.Buttons.RightStick);
+        displayGamepadInputText.text += "\n--- Triggers ---\n";
+        displayGamepadInputText.text += string.Format("Left Trigger: {0}\nRightTrigger: {1}\n", state.Triggers.Left, state.Triggers.Right);
+        displayGamepadInputText.text += "\n--- D-Pad ---\n";
+        displayGamepadInputText.text += string.Format("Up: {0} | Right: {1} | Down: {2} | Left: {3}\n", state.DPad.Up, state.DPad.Right, state.DPad.Down, state.DPad.Left);
+    }
+}
